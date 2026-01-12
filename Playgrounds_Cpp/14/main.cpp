0125e4a848df0bb1bd82967d5bfe4e1f6c0a1091
Aashish Raj <iamtheaashish@gmail.com>
2026-01-12 14:45:55 +0530

    Did pracitce from learncpp and streamed - 12 Jan

--- Playgrounds_Cpp/14/main.cpp
+++ Playgrounds_Cpp/14/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
+	int num1{},num2{};
 	
-return 0;
+	cout << "Enter an integer: ";
+	cin >> num1;
+
+	cout << "Enter another integer: ";
+	cin >> num2;
+
+	cout << num1 << " + " << num2 << " is " << num1 + num2 << ".\n";
+	cout << num1 << " - " << num2 << " is " << num1 - num2 << ".\n";	
+	return 0;
 }

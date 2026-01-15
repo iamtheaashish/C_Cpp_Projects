9ad9cc58260ec327eb7b95dd57cb9ce336cec941
Aashish Raj <iamtheaashish@gmail.com>
2026-01-15 19:18:05 +0530

    HUGE CHANGES

---
+++ CPP_Progression/01_CLI_Data_Collector/Using_Function/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+void getInput();
+void giveOutput();
+
+ getInput()
+{
+    std::cout << "Enter name:"
+}
+
+
+int main()
+{
+    std::string naam{getInput()};
+    giveOutput();
+    return 0;
+}

fc6d22ee2bc6f43fa1203999938b6412a494d3c0
Aashish Raj <iamtheaashish@gmail.com>
2026-01-21 19:35:48 +0530

    Implement basic calculator functionality with user input for double values and operators

--- files/Playgrounds_Cpp/59/main.cpp
+++ files/Playgrounds_Cpp/59/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+double getDouble()
+{
+    std::cout << "Enter a double value: ";
+    double x{};
+    std::cin >> x;
+
+    return x;
+}
+
+char getOperator()
+{
+	std::cout << "Enter +, -, *, or /: ";
+	char operat{};
+	std::cin >> operat;
+
+	return operat;
+}
+
+void printResult(double x, char operat, double y)
+{
+	double result{};
+
+	if ( operat == '+' )
+		result = x + y;
+	else if ( operat == '-')
+		result = x - y;
+	else if ( operat == '*' )
+		result = x * y;
+	else if ( operat == '/' )
+		result = x / y;
+	else
+		return;
+	std::cout << x << ' ' << operat << ' ' << y << " is " << result << '\n';
+}
 
 int main()
 {
+    double a{ getDouble() };
+    double b{ getDouble() };
 
+    char operat{ getOperator() };
+
+    printResult(a,operat,b);    
     return 0;
 }

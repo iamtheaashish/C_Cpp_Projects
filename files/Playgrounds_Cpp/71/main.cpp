0944a6b64bd83d2d57ec1ea720ba6c6dbd3364d1
Aashish Raj <iamtheaashish@gmail.com>
2026-01-25 18:45:30 +0530

    5.x quiz question 2

---
+++ files/Playgrounds_Cpp/71/main.cpp
@@ -0,0 +1,27 @@
+// Find 3 issues in the following code:
+
+
+/*
+#include <cstdint> // for std::uint8_t
+#include <iostream>
+
+int main()
+{
+  std::cout << "How old are you?\n";
+
+  std::uint8_t age{};
+  std::cin >> age;
+
+  std::cout << "Allowed to drive a car in Texas: ";
+
+  if (age >= 16)
+      std::cout << "Yes";
+  else
+      std::cout << "No";
+
+  std::cout << '.\n';
+
+  return 0;
+}
+
+*/

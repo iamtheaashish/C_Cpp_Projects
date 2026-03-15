49b233c38129b36417ea25f145e75aa3a55b889f
iamtheaashish <iamtheaashish@gmail.com>
2026-03-15 20:23:42 +0530

    program defined user defined types

---
+++ files/Playgrounds_Cpp/161/Fraction.cpp
@@ -0,0 +1,8 @@
+#include "Fraction.h"
+
+int main()
+{
+	Fraction f {3, 4};
+
+	return 0;
+}
---
+++ files/Playgrounds_Cpp/161/struct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct Fraction
+{
+	int numerator{};
+	int denominator{};
+};
+// don't forget to end your type definitions with a semicolon.
+
+int main()
+{
+	Fraction f {3,4};
+	return 0;
+}

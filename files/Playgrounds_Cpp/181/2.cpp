eb53db55a81009aade6647acb0373b5e68ce680b
iamtheaashish <iamtheaashish@gmail.com>
2026-03-28 10:23:47 +0530

    return by reference in member functions

---
+++ files/Playgrounds_Cpp/181/2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+class Employee
+{
+	std::string m_name{};
+	public:
+	void setName(std::string_view name) { m_name = name; }
+	const auto& getName() const { return m_name; }
+};
+
+int main()
+{
+	Employee joe{};
+	joe.setName("Joe");
+	std::cout << joe.getName();
+
+	return 0;
+}
